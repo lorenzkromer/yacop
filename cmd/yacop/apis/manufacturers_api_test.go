ad36a598ebd4f91b14484156d4719d3e07fa6b26
Lorenz Kromer <lorenz.kromer@gmail.com>
2021-04-11 14:49:04 +0200

    [testing] implement tests for basic crud functionalities

--- cmd/yacop/apis/manufacturers_api_test.go
+++ cmd/yacop/apis/manufacturers_api_test.go
@@ -1,7 +1,12 @@
 package apis
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/fitchlol/yacop/cmd/yacop/models"
 	"github.com/fitchlol/yacop/cmd/yacop/test_data"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"testing"
 )
@@ -24,4 +29,120 @@ func TestManufacturers(t *testing.T) {
 			path + "/all.json",
 		},
 	})
+}
+
+func TestManufacturerCreate(t *testing.T) {
+	path := test_data.GetTestCaseFolder() + "/manufacturers"
+
+	manufacturerCreateContent, err := ioutil.ReadFile(path + "/requests/manufacturers_create_new_manufacturer.json")
+	if err != nil {
+		panic(err)
+	}
+	manufacturerCreateData := new(bytes.Buffer)
+
+	if err := json.Compact(manufacturerCreateData, manufacturerCreateContent); err != nil {
+		panic(err)
+	}
+	manufacturerCreateHeaders := []requestHeader{{key: "Content-Type", value: "application/json"}}
+
+	router := newRouter()
+	res := testAPI(router, "POST", "/manufacturers", "/manufacturers", ManufacturerCreate, manufacturerCreateData.String(), manufacturerCreateHeaders)
+	assert.Equal(t, http.StatusCreated, res.Code, "create new manufacturer")
+	var createdManufacturer models.Manufacturer
+	err = json.Unmarshal([]byte(res.Body.String()), &createdManufacturer)
+	if err != nil {
+		panic(err)
+	}
+	assert.NotNil(t, createdManufacturer.Name)
+}
+
+func TestManufacturerUpdate(t *testing.T) {
+	path := test_data.GetTestCaseFolder() + "/manufacturers"
+
+	manufacturerUpdateContent, err := ioutil.ReadFile(path + "/requests/manufacturers_update.json")
+	if err != nil {
+		panic(err)
+	}
+	manufacturerUpdateData := new(bytes.Buffer)
+
+	if err := json.Compact(manufacturerUpdateData, manufacturerUpdateContent); err != nil {
+		panic(err)
+	}
+	manufacturerUpdateHeaders := []requestHeader{{key: "Content-Type", value: "application/json"}}
+
+	router := newRouter()
+	res := testAPI(
+		router,
+		"PUT",
+		"/manufacturers/:id",
+		"/manufacturers/00000000-0000-0000-0000-000000000010",
+		ManufacturerUpdate,
+		manufacturerUpdateData.String(),
+		manufacturerUpdateHeaders,
+	)
+	assert.Equal(t, http.StatusOK, res.Code, "update existing manufacturer")
+	var updatedManufacturer models.Manufacturer
+	err = json.Unmarshal([]byte(res.Body.String()), &updatedManufacturer)
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, "BMW", updatedManufacturer.Name)
+}
+
+func TestManufacturerUpdateUnknown(t *testing.T) {
+	path := test_data.GetTestCaseFolder() + "/manufacturers"
+
+	manufacturerUpdateContent, err := ioutil.ReadFile(path + "/requests/manufacturers_update.json")
+	if err != nil {
+		panic(err)
+	}
+	manufacturerUpdateData := new(bytes.Buffer)
+
+	if err := json.Compact(manufacturerUpdateData, manufacturerUpdateContent); err != nil {
+		panic(err)
+	}
+	manufacturerUpdateHeaders := []requestHeader{{key: "Content-Type", value: "application/json"}}
+
+	router := newRouter()
+	res := testAPI(
+		router,
+		"PUT",
+		"/manufacturers/:id",
+		"/manufacturers/10000000-0000-0000-0000-000000000000",
+		ManufacturerUpdate,
+		manufacturerUpdateData.String(),
+		manufacturerUpdateHeaders,
+	)
+	assert.Equal(t, http.StatusNotFound, res.Code, "update unknown manufacturer")
+}
+
+func TestManufacturerDelete(t *testing.T) {
+	router := newRouter()
+	var emptyHeaders []requestHeader
+	res := testAPI(
+		router,
+		"DELETE",
+		"/manufacturers/:id",
+		"/manufacturers/00000000-0000-0000-0000-000000000010",
+		ManufacturerDelete,
+		"",
+		emptyHeaders,
+	)
+	assert.Equal(t, http.StatusNoContent, res.Code, "delete existing manufacturer")
+	assert.Equal(t, "", res.Body.String())
+}
+
+func TestManufacturerDeleteUnknown(t *testing.T) {
+	router := newRouter()
+	var emptyHeaders []requestHeader
+	res := testAPI(
+		router,
+		"DELETE",
+		"/manufacturers/:id",
+		"/manufacturers/10000000-0000-0000-0000-000000000000",
+		ManufacturerDelete,
+		"",
+		emptyHeaders,
+	)
+	assert.Equal(t, http.StatusNotFound, res.Code, "delete unknown manufacturer")
 }
--- cmd/yacop/apis/vehicles_api_test.go
+++ cmd/yacop/apis/vehicles_api_test.go
@@ -1,7 +1,12 @@
 package apis
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/fitchlol/yacop/cmd/yacop/models"
 	"github.com/fitchlol/yacop/cmd/yacop/test_data"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"testing"
 )
@@ -24,4 +29,124 @@ func TestVehicles(t *testing.T) {
 			path + "/all.json",
 		},
 	})
+}
+
+func TestVehicleCreate(t *testing.T) {
+	path := test_data.GetTestCaseFolder() + "/vehicles"
+
+	vehicleCreateContent, err := ioutil.ReadFile(path + "/requests/vehicles_create_new_vehicle.json")
+	if err != nil {
+		panic(err)
+	}
+	vehicleCreateData := new(bytes.Buffer)
+
+	if err := json.Compact(vehicleCreateData, vehicleCreateContent); err != nil {
+		panic(err)
+	}
+	vehicleCreateHeaders := []requestHeader{{key: "Content-Type", value: "application/json"}}
+
+	router := newRouter()
+	res := testAPI(router, "POST", "/vehicles", "/vehicles", VehicleCreate, vehicleCreateData.String(), vehicleCreateHeaders)
+	assert.Equal(t, http.StatusCreated, res.Code, "create new vehicle")
+	var createdVehicle models.Vehicle
+	err = json.Unmarshal([]byte(res.Body.String()), &createdVehicle)
+	if err != nil {
+		panic(err)
+	}
+	assert.NotNil(t, createdVehicle.WeightInKilograms)
+	assert.NotNil(t, createdVehicle.MaximumKilowatts)
+	assert.NotNil(t, createdVehicle.MaximumKilometersPerHour)
+	assert.NotNil(t, createdVehicle.FullName)
+	assert.NotNil(t, createdVehicle.Manufacturer)
+}
+
+func TestVehicleUpdate(t *testing.T) {
+	path := test_data.GetTestCaseFolder() + "/vehicles"
+
+	vehicleUpdateContent, err := ioutil.ReadFile(path + "/requests/vehicles_update.json")
+	if err != nil {
+		panic(err)
+	}
+	vehicleUpdateData := new(bytes.Buffer)
+
+	if err := json.Compact(vehicleUpdateData, vehicleUpdateContent); err != nil {
+		panic(err)
+	}
+	vehicleUpdateHeaders := []requestHeader{{key: "Content-Type", value: "application/json"}}
+
+	router := newRouter()
+	res := testAPI(
+		router,
+		"PUT",
+		"/vehicles/:id",
+		"/vehicles/00000000-0000-0000-0000-000000000001",
+		VehicleUpdate,
+		vehicleUpdateData.String(),
+		vehicleUpdateHeaders,
+	)
+	assert.Equal(t, http.StatusOK, res.Code, "update existing vehicle")
+	var updatedVehicle models.Vehicle
+	err = json.Unmarshal([]byte(res.Body.String()), &updatedVehicle)
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, "A6 3.0 TDI", updatedVehicle.FullName)
+}
+
+func TestVehicleUpdateUnknown(t *testing.T) {
+	path := test_data.GetTestCaseFolder() + "/vehicles"
+
+	vehicleUpdateContent, err := ioutil.ReadFile(path + "/requests/vehicles_update.json")
+	if err != nil {
+		panic(err)
+	}
+	vehicleUpdateData := new(bytes.Buffer)
+
+	if err := json.Compact(vehicleUpdateData, vehicleUpdateContent); err != nil {
+		panic(err)
+	}
+	vehicleUpdateHeaders := []requestHeader{{key: "Content-Type", value: "application/json"}}
+
+	router := newRouter()
+	res := testAPI(
+		router,
+		"PUT",
+		"/vehicles/:id",
+		"/vehicles/10000000-0000-0000-0000-000000000000",
+		VehicleUpdate,
+		vehicleUpdateData.String(),
+		vehicleUpdateHeaders,
+	)
+	assert.Equal(t, http.StatusNotFound, res.Code, "update unknown vehicle")
+}
+
+func TestVehicleDelete(t *testing.T) {
+	router := newRouter()
+	var emptyHeaders []requestHeader
+	res := testAPI(
+		router,
+		"DELETE",
+		"/vehicles/:id",
+		"/vehicles/00000000-0000-0000-0000-000000000001",
+		VehicleDelete,
+		"",
+		emptyHeaders,
+	)
+	assert.Equal(t, http.StatusNoContent, res.Code, "delete existing vehicle")
+	assert.Equal(t, "", res.Body.String())
+}
+
+func TestVehicleDeleteUnknown(t *testing.T) {
+	router := newRouter()
+	var emptyHeaders []requestHeader
+	res := testAPI(
+		router,
+		"DELETE",
+		"/vehicles/:id",
+		"/vehicles/10000000-0000-0000-0000-000000000000",
+		VehicleDelete,
+		"",
+		emptyHeaders,
+	)
+	assert.Equal(t, http.StatusNotFound, res.Code, "delete unknown vehicle")
 }
